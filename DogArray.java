7f66ac92f0c874a9787b717bf27b859aac3461ba
MaclaineS <69940704+MaclaineS@users.noreply.github.com>
2021-01-29 10:29:30 -0600

    Completed getYoungetIndex & getYoungestName methods

--- DogArray.java
+++ DogArray.java
@@ -6,14 +6,23 @@ public class DogArray
      */
     public static int getYoungestIndex(Dog[] dogs)
     {
-        return -1;
+      int index=0;
+      for(int i=0;i<dogs.length;i++){
+        if(dogs[i].getAge()<dogs[index].getAge()){
+          index=i;
+        }
+      }
+      return index;
     }
+      
+    
 
     /**
      *  Returns the name of the youngest dog in the given array
      */
     public static String getYoungestName(Dog[] dogs)
     {
-        return "";
+      int index=getYoungestIndex(dogs);
+      return dogs[index].getName();
     }
 }
